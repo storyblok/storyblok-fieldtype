fa6e0c098593c53469625642573941cba5c41179
Alexander Feiglstorfer <delooks@gmail.com>
2017-08-02 15:00:00 +0200

    adds api prop to enable api access in plugin development

--- src/vendor/main.js
+++ src/vendor/main.js
@@ -5,14 +5,42 @@ new Vue({
   data() {
     return {
       schema: {},
-      model: undefined
+      model: undefined,
+      spaceId: '40882',
+      loggedIn: false
     }
   },
   el: 'body',
   components: { Plugin },
+  ready() {
+    this.login()
+  },
   methods: {
     update(v) {
       this.model = v
+    },
+
+    api(path) {
+      return VueGlobal.app.$resource(path)
+    },
+
+    login() {
+      let user = VueGlobal.app.$resource('users/{id}', {}, {
+        login: {method: 'POST', url: 'users/login'
+      }})
+
+      user.login({email: 'storyblok.com+fieldtype@gmail.com', password: 'fieldtype'})
+        .then((res) => {
+          window.VueHttpOptions.headers['Authorization'] = res.data.access_token
+          window.localStorage.setItem('token', res.data.access_token)
+          window.localStorage.setItem('role', res.data.role || 'normal')
+          window.localStorage.setItem('userId', res.data.user_id)
+
+          this.loggedIn = true
+        })
+        .catch((err) => {
+          console.error(err)
+        })
     }
   }
 })
