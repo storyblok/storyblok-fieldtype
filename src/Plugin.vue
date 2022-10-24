41245c1975cd5a145cea97bfd0c7391dec3d6c83
Nathan Hensher <nathan.hensher@boughtbymany.com>
2022-10-24 14:48:47 +0100

    feat: block WebPage type from structured data schema

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,52 +1,58 @@
 <!-- eslint-disable vuejs-accessibility/form-control-has-label -->
 <template>
   <div class="sb-select-wrap">
-    <div class="sb-group-button">
-      <form
-        method="post"
-        target="_blank"
-        action="https://search.google.com/test/rich-results"
-      >
-        <SbButton
-          icon="new-window"
-          has-icon-only
-          icon-description="Rich Results Test"
-          size="small"
-          type="submit"
-        />
-        <textarea
-          name="code_snippet"
-          :value="JSON.stringify(model.schema)"
-          class="hide"
-          hidden
-        />
-      </form>
-      <form method="post" target="_blank" action="https://validator.schema.org">
-        <SbButton
-          icon="new-window"
-          has-icon-only
-          icon-description="Structured Data Testing Tool"
-          size="small"
-          type="submit"
-        />
-        <textarea
-          name="code"
-          :value="JSON.stringify(model.schema)"
-          class="hide"
-          hidden
-        />
-      </form>
+    <div class="sb-toolbar">
+      <div class="sb-group-button">
+        <form
+          method="post"
+          target="_blank"
+          action="https://search.google.com/test/rich-results"
+        >
+          <SbButton
+            icon="new-window"
+            has-icon-only
+            icon-description="Rich Results Test"
+            size="small"
+            type="submit"
+          />
+          <textarea
+            name="code_snippet"
+            :value="JSON.stringify(model.schema)"
+            class="hide"
+            hidden
+          />
+        </form>
+        <form
+          method="post"
+          target="_blank"
+          action="https://validator.schema.org"
+        >
+          <SbButton
+            icon="new-window"
+            has-icon-only
+            icon-description="Structured Data Testing Tool"
+            size="small"
+            type="submit"
+          />
+          <textarea
+            name="code"
+            :value="JSON.stringify(model.schema)"
+            class="hide"
+            hidden
+          />
+        </form>
 
-      <SbGroupButton size="small" variant="primary" has-spaces>
-        <SbButton
-          icon="delete"
-          has-icon-only
-          variant="dark"
-          icon-description="reset"
-          type="button"
-          @click="resetJson"
-        />
-      </SbGroupButton>
+        <SbGroupButton size="small" variant="primary" has-spaces>
+          <SbButton
+            icon="delete"
+            has-icon-only
+            variant="dark"
+            icon-description="reset"
+            type="button"
+            @click="resetJson"
+          />
+        </SbGroupButton>
+      </div>
     </div>
     <input
       ref="clone"
@@ -68,6 +74,7 @@
     <div v-if="error" class="sb-error">
       {{ error }}
     </div>
+    <hr />
   </div>
 </template>
 
@@ -87,9 +94,22 @@ export default {
       error: '',
     }
   },
+  computed: {
+    schemaType() {
+      if (!this.model.schema) {
+        return ''
+      }
+      return this.model.schema['@type']
+    },
+  },
   watch: {
     model: {
       handler(value) {
+        if (value.blockedSchema[value.schema['@type']]) {
+          this.error = `${value.schema['@type']} is a blocked schema type`
+          return
+        }
+
         this.$emit('changed-model', value)
       },
       deep: true,
@@ -99,6 +119,9 @@ export default {
     initWith() {
       return {
         plugin: 'schema-markup',
+        blockedSchema: {
+          WebPage: true,
+        },
       }
     },
     onJsonChange() {
@@ -123,13 +146,27 @@ export default {
   color: #ff6159;
   padding: 10px 0;
 }
+.sb-toolbar {
+  display: flex;
+  justify-content: flex-end;
+}
+.sb-options > h3 {
+  margin: 20px 0;
+}
+.sb-options-group {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+.sb-options-group__checkbox {
+  margin-bottom: 16px;
+}
 .sb-group-button {
-  display: flex !important;
-  justify-content: right;
-  margin-bottom: 15px;
+  align-items: flex-end;
 }
 .sb-group-button button {
-  margin-right: 10px;
+  margin-left: 10px;
+  margin-bottom: 10px;
 }
 div.jsoneditor {
   border: solid 2px #09b3af;
